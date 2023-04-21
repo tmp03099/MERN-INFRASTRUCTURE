4652e90e9179fc27861a5cb721d192c3b1fd0a45
My Phung Tieu <tmp03099@gmail.com>
2023-04-21 10:06:46 -0400

    created token

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,13 +1,42 @@
-// We will use
-// * SignUpForm.jsx <--> users-service.js <--> users-api.js <-Internet-> server.js (Express)
+// * We will use a src/utilities/users-service.js module to organize functions used to sign-up, log in, log out, etc.
 
-// * handleSubmit <--> [signUp]-users-service.js <--> [signUp]-users-api.js <-Internet-> server.js (Express)
+//* SignUpForm.jsx <--> users-service.js <--> users-api.js <-Internet-> server.js (Express)
 
-import * as usersAPI from "./users-api";
+//* handleSubmit <--> [signUp]-users-service <--> [signUp]-users-api <-Internet-> server.js (Express)
+
+import * as usersApi from "./users-api";
+
+//* Get Token
+export function getToken() {
+  const token = localStorage.getItem("token");
+  if (!token) return null;
+
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  console.log(payload);
+
+  //if token is expried
+  if (payload.exp < Date.now() / 1000) {
+    localStorage.removeItem("token");
+    return null;
+  }
+
+  //token is valid
+  return token;
+}
+
+//* Get User
+export function getUser() {
+  const token = getToken();
+  return token ? JSON.parse(atob(token.split(".")[1])).user : null;
+}
+
 export async function signUp(userData) {
   // Delegate the network request code to the users-api.js API module
   // which will ultimately return a JSON Web Token (JWT)
-  //console.log('[Form sign up function]', userData)
-  const token = await usersAPI.signUp(userData);
+  // console.log('[From SignUP function]', userData);
+  const token = await usersApi.signUp(userData);
+  // saves token to localStorage
+  localStorage.setItem("token", token);
+
   return token;
 }
